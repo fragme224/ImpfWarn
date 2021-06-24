444d593c7e92be58c1e0ea8d9d9a4c3153cf26e1
Waldemar K <fraqqy@gmail.com>
2021-06-25 01:51:01 +0200

    Telegram as messenger

--- ImpfWarn_loop.php
+++ ImpfWarn_loop.php
@@ -1,25 +1,32 @@
 <?php
 
-while(true) {
+$parameters = [
+    'chat_id' => '921746690',
+    'text' => 'Es gibt einen IMPFTERMIN!!!'
+];
+
+while (true) {
     $url = 'https://www.impfportal-niedersachsen.de/portal/rest/appointments/findVaccinationCenterListFree/38126?stiko=&count=1&birthdate=600001200000';
 
     $response = file_get_contents($url);
     $response = json_decode($response, true);
-    
-    
+
+
     if (isset($response['resultList'][0]['outOfStock'])) {
         if ($response['resultList'][0]['outOfStock'] != 1) {
             echo "Termin vorhanden! \n";
-            sendEmail();
+            // sendEmail();
+            sendTelegramMessage('sendMessage', $parameters);
         } else {
             // sendEmailError();
+            // sendTelegramMessage('sendMessage', $parameters);
         }
     } else {
         // sendEmailError();
+        // sendTelegramMessage('sendMessage', $parameters);
     }
 
     sleep(30);
- 
 }
 
 
@@ -45,6 +52,27 @@ function sendEmailError()
     mail($to, $subject, $txt, $headers);
 }
 
-function writeLog($msg) {
+
+function writeLog($msg)
+{
     // TODO: 
+}
+
+
+function sendTelegramMessage($method, $parameters)
+{
+    $botToken = '1858527465:AAH8gGuTXFQohDkYHK8j0Gyu5jxCHyqIdz8';
+    $url = 'https://api.telegram.org/bot' . $botToken . '/' . $method;
+
+    if (!$curl = curl_init()) {
+        exit();
+    }
+
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $parameters);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $output = curl_exec($curl);
+    return $output;
 }
